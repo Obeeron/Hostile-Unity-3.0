1e1a778ceeccb714662605e63cabf9efcc8874b7
Obeeron <obeeron@hotmail.com>
2020-01-26 20:38:43 +0100

    Basic main menu

---
+++ Hostile/Assets/Scripts/Menus/Main.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Main : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject playBtn;
+
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject optionsPanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting to master server..");
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Now connected to " + PhotonNetwork.CloudRegion + " server.");
+        playBtn.SetActive(true);
+    }
+    public void OnPlayBtnClick()
+    {
+        
+    }
+
+    public void OnOptionsBtnClick()
+    {
+        optionsPanel.SetActive(true);
+        mainPanel.SetActive(false);
+    }
+
+    public void OnQuitBtnClick()
+    {
+        Debug.Log("Closing application");
+        PhotonNetwork.Disconnect();
+        Application.Quit();
+    }
+}
---
+++ Hostile/Assets/Scripts/Menus/Options.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+#pragma warning disable 649
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject optionsPanel;
+#pragma warning restore 649
+
+    public void OnBackBtnClick()
+    {
+        mainPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+    }
+}
