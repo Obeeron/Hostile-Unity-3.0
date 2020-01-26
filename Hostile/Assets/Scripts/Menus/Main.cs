f54d625313c9fc87bb0758f4a09904611807ea2f
Obeeron <obeeron@hotmail.com>
2020-01-26 20:51:45 +0100

    (fix main menu)
    j'avais oublié de save la scene ahah

--- Hostile/Assets/Scripts/Menus/Main.cs
+++ Hostile/Assets/Scripts/Menus/Main.cs
@@ -4,11 +4,13 @@ using Photon.Realtime;
 
 public class Main : MonoBehaviourPunCallbacks
 {
+#pragma warning disable 649
     [SerializeField] private GameObject playBtn;
 
     [SerializeField] private GameObject mainPanel;
     [SerializeField] private GameObject optionsPanel;
-    
+#pragma warning restore 649
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,10 +26,10 @@ public class Main : MonoBehaviourPunCallbacks
     }
     public void OnPlayBtnClick()
     {
-        
+        Debug.Log("Play button clicked");
     }
 
-    public void OnOptionsBtnClick()
+    public void OnSettingsBtnClick()
     {
         optionsPanel.SetActive(true);
         mainPanel.SetActive(false);
