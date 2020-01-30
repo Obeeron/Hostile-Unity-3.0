a86aee1a65445969d0e2776c482454b3cc8ee7b9
n <baptiste.nehme@epita.fr>
2020-01-30 21:57:33 +0100

    added Menu/options/controlls for bindings
    only pannel, bindings.cs not operational yet

---
+++ Hostile/Assets/Scripts/Menus/Bindings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Bindings : MonoBehaviour
+{
+#pragma warning disable 649
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject controllsPannel;
+#pragma warning restore 649
+
+    void RemapButtonClicked(InputAction actionToRebind)
+    {
+        var rebindOperation = actionToRebind
+            .PerformInteractiveRebinding().Start();
+    }
+
+    public void OnBackBtnClick()
+    {
+        optionsPanel.SetActive(true);
+        controllsPannel.SetActive(false);
+    }
+}
--- Hostile/Assets/Scripts/Menus/Options.cs
+++ Hostile/Assets/Scripts/Menus/Options.cs
@@ -7,11 +7,18 @@ public class Options : MonoBehaviour
 #pragma warning disable 649
     [SerializeField] private GameObject mainPanel;
     [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject controllsPannel;
 #pragma warning restore 649
 
     public void OnBackBtnClick()
     {
         mainPanel.SetActive(true);
         optionsPanel.SetActive(false);
+    }
+
+    public void OnControllsBtnClick()
+    {
+        controllsPannel.SetActive(true);
+        mainPanel.SetActive(false);
     }
 }
