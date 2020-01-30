e4d3ea6653a1b9be86a12c12390975fedb448be8
Obeeron <obeeron@hotmail.com>
2020-01-30 01:00:18 +0100

    Unity's Standard Assets + SandboxObee
    Basic terrain with grass & player controller

---
+++ Hostile/Assets/Sandbox/Obee/Terrain/Camera_Obee.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Obee : MonoBehaviour
+{
+    Transform playerCamera;
+
+    public float mouseSensitivity = 150f;
+    public float smoothness;
+
+    float yRotation;
+
+    private Vector2 mouseRotation;
+
+    private void Start()
+    {
+        yRotation = 0f;
+        Cursor.lockState = CursorLockMode.Locked;
+        playerCamera = GetComponentInChildren<Camera>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newMouseRotation = new Vector2( Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime,
+                                                Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime);
+
+        mouseRotation.x = Mathf.Lerp(newMouseRotation.x, mouseRotation.x, smoothness);
+        mouseRotation.y = Mathf.Lerp(newMouseRotation.y, mouseRotation.y, smoothness);
+
+        yRotation = Mathf.Clamp(yRotation - mouseRotation.y, -90f, 90f);
+
+        transform.Rotate(Vector3.up * mouseRotation.x);
+        playerCamera.transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+    }
+}
---
+++ Hostile/Assets/Sandbox/Obee/Terrain/PlayerMovement_Obee.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+public class PlayerMovement_Obee : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float normalSpeed = 5f;
+    public float runningSpeed = 9f;
+    private float speed;
+    private Vector3 fallingVelocity;
+
+    bool grounded;
+    public float jumpHeight = 1f;
+
+    public float groundLerp;
+    public float airLerp;
+    public float airSpeedMultiplier;
+    private Vector3 motion;
+
+    private void Start()
+    {
+        grounded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = controller.isGrounded;           //Check if player is on the ground
+
+        float x = Input.GetAxisRaw("Vertical");     //Get 2D movements inputs
+        float y = Input.GetAxisRaw("Horizontal");
+        
+        Vector3 direction = (x * transform.forward + y * transform.right).normalized;       //normalized direction to avoid faster diagonal movement
+
+        float lerpValue = (grounded) ? groundLerp : airLerp;                                //Lerp motion to smooth start and stop of player motion
+        motion = Vector3.Lerp(direction * speed * Time.deltaTime, motion, lerpValue);
+
+        if (grounded)
+        {
+            speed = (Input.GetKey(KeyCode.LeftShift)) ? runningSpeed : normalSpeed;  //Set waking speed
+
+            if (Input.GetButtonDown("Jump"))        //Get Jump Input
+                StartCoroutine(Jump());
+            else if (fallingVelocity.y < 0f)        //Resets fallingVelocity if player fell on the ground and isn't jumping
+                fallingVelocity.y = -2f;
+        }
+    }
+
+    private IEnumerator Jump()
+    {
+        controller.slopeLimit = 90f;            //preventing wall jitter
+        speed *= airSpeedMultiplier;            //reducing midair speed
+        fallingVelocity.y = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+
+        do
+        {
+            yield return null;
+        } while (!grounded);
+
+        controller.slopeLimit = 45f;
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (!grounded)
+            fallingVelocity += (Physics.gravity * Time.deltaTime);  //Update fallingVelocity 
+
+        controller.Move(motion);                                    //Apply summed velocity
+        controller.Move(fallingVelocity * Time.deltaTime);
+    }
+}
