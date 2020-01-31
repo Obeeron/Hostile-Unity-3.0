162a131adda9c6a8e9b68b39c838fe0def3a5ed7
N2S <nathan.de-souza@epita.fr>
2020-01-31 15:16:30 +0100

    Upgraded prefabPlayer

---
+++ Hostile/Assets/Sandbox/Nath/Script/FarmSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmSystem : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.Play("Short_Grab");
+        }
+
+    }
+}
