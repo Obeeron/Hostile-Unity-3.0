fd1877dc3eb9c547e2df8982fe7b20ab65ab060c
N2S <nathan.de-souza@epita.fr>
2020-01-31 14:13:06 +0100

    Started FightingSystem

---
+++ Hostile/Assets/Sandbox/Nath/Script/FightSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightSystem : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.Play("pickaxe_hit");
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Touched");
+    }
+}
