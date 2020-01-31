25c64851e63ea8ae9c8bf2b5909e50f31f5f9eb4
Obeeron <obeeron@hotmail.com>
2020-01-31 22:50:29 +0100

    Basic multi scene

---
+++ Hostile/Assets/Sandbox/Obee/GameSetupManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using System;
+
+public class GameSetupManager : MonoBehaviourPunCallbacks
+{
+#pragma warning disable 649
+    [SerializeField] Vector3 spawnPoint;
+#pragma warning restore 649
+    private PhotonView phView;
+
+    public void Start()
+    {
+        phView = GetComponent<PhotonView>();
+        Debug.Log("Room Joined");
+        CreatePlayer();
+    }
+
+    private void CreatePlayer()
+    {
+        PhotonNetwork.Instantiate("NetworkSimplePlayer", spawnPoint, Quaternion.identity);
+        Debug.Log("Avatar created.");
+    }
+}
---
+++ Hostile/Assets/Sandbox/Obee/PlayerNetworkController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class PlayerNetworkController : MonoBehaviour
+{
+#pragma warning disable 649
+    [SerializeField] MonoBehaviour[] ignoredScripts;
+#pragma warning restore 649
+    PhotonView phView;
+
+    void Start()
+    {
+        phView = GetComponent<PhotonView>();
+        if (!phView.IsMine)
+        {
+            foreach (var script in ignoredScripts)
+                script.enabled = false;
+            GetComponentInChildren<Camera>().enabled = false;
+        }
+    }
+}
