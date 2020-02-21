89dc705a2d5cf27d4d70affb964852f27eedd5bf
N2S <nathan.de-souza@epita.fr>
2020-02-21 14:00:31 +0100

    Debug.Log RPC call on hit

--- Hostile/Assets/Sandbox/Nath/Script/Fight/TriggerCollider.cs
+++ Hostile/Assets/Sandbox/Nath/Script/Fight/TriggerCollider.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using Photon.Pun;
+using UnityEngine.EventSystems;
 
 public class TriggerCollider : MonoBehaviour
 {
+    //public UnityEvent onHit;
+    [SerializeField]
+    private PlayerData playerData;
     private void OnTriggerEnter(Collider other)
     {
         CharacterController main = this.GetComponentInParent<CharacterController>();
         if (other != main) // On vérifie qu'on ne se tappe pas soi-même
         {
+            //PointerEventData pointerData = data as PointerEventData;
+            PhotonView PV = PhotonView.Get(this);
+            PV.RPC("GetHit", RpcTarget.MasterClient, playerData.Strength,other);
             Debug.Log("Touched");
             /*if (other.GetComponent<Entity>() != null)
             {
@@ -17,5 +26,11 @@ public class TriggerCollider : MonoBehaviour
                 ennemy.getHit(player.Damage); //getHit() -->
             }*/
         }
+    }
+
+    [PunRPC]
+    void GetHit(int dmg,Collider other)
+    {
+        Debug.Log("ur getting hit");
     }
 }
