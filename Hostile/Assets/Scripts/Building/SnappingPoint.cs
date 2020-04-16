377b048ad9cf3008dc82be856bcb12068000ff37
Obeeron <Obeeron@hotmail.com>
2020-04-16 14:36:42 +0200

    Build local (wt scripts)

---
+++ Hostile/Assets/Scripts/Building/SnappingPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BuildingSystem
+{
+    public class SnappingPoint : MonoBehaviour
+    {
+        public Building buildingParent;
+        public List<BuildElement> buildElements;
+        public int surroundingPillars = 0;
+        public bool used = false;
+    }
+}
