34c692a69d900e19378333e28a6029acb711567a
Baptiste Nehmé <baptiste.nehme@epita.fr>
2020-04-22 17:51:45 +0200

    Wher did the pannel went? (bindings)

--- Hostile/Assets/Scripts/UI/Menus/Bindings.cs
+++ Hostile/Assets/Scripts/UI/Menus/Bindings.cs
@@ -10,8 +10,12 @@ public class Bindings : MonoBehaviour
 {
 
     [Tooltip("list of rebindOperation fro reset all. Add every rebindOperation to this.")]
+    #pragma warning disable 649
     [SerializeField] private List<RebindOperation> resetList;
     [SerializeField] private InputActionReference action;
+    [SerializeField] private GameObject currentPannel;
+    [SerializeField] private GameObject otherPannel;
+    #pragma warning restore 649
 
     public void ResetAll()
     {
@@ -19,5 +23,11 @@ public class Bindings : MonoBehaviour
         {
             rb.reset();
         }
+    }
+
+    public void change()
+    {
+        currentPannel.SetActive(false);
+        otherPannel.SetActive(true);
     }
 }
